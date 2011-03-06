e913257d8d261eec5b2df732f4dac9370fad96d9
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-03-06 15:41:47 -0800

    Php mode works

--- defunkt/php.el
+++ defunkt/php.el
@@ -1,1 +1,5 @@
 ; php
+(add-to-list 'load-path "~/.emacs.d/vendor/php-mode.el")
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(setq auto-mode-alist (cons '("\\.php" . php-mode) auto-mode-alist))
+
