340e4cdc9dc33c63e49b63ce8d3577714824c5ab
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-04-06 11:17:57 -0700

    Update some global changes with rmm5t changes

--- defunkt/global.el
+++ defunkt/global.el
@@ -1,27 +1,54 @@
-(setq tags-file-name "TAGS")
-(setq-default tab-width 4)
-(setq-default indent-tabs-mode nil)
+;;;; Stolen from rmm5t
+
+;; Use command as the meta key
+(setq ns-command-modifier (quote meta))
+
+;; Don't show the startup screen
+(setq inhibit-startup-message t)
+
+;; "y or n" instead of "yes or no"
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Highlight regions and add special behaviors to regions.
+;; "C-h d transient" for more info
+(setq transient-mark-mode t)
+
+;; Prevent the annoying beep on errors
+(setq visible-bell t)
+
+;; Display line and column numbers
+(setq line-number-mode    t)
+(setq column-number-mode  t)
+
+;; Emacs gurus don't need no stinking scroll bars
+(when (fboundp 'toggle-scroll-bar)
+  (toggle-scroll-bar -1))
+
+;; Explicitly show the end of a buffer
+(set-default 'indicate-empty-lines t)
+
+;; Line-wrapping
+(set-default 'fill-column 80)
+
+;; Make sure all backup files only live in one place
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+
+;; Gotta see matching parens
+(show-paren-mode t)
+
+;; Don't truncate lines
+(setq truncate-lines t)
+(setq truncate-partial-width-windows nil)
+
+;; Mac setting?
 (setq mac-emulate-three-button-mouse nil)
-;;(setq cua-highlight-region-shift-only t)
+
+;; Set file encoding
 (prefer-coding-system 'utf-8)
-;;(setq vc-handled-backends nil)
 
-
-;; Option doesn't work, command is meta
+;; Use Command as the meta key
 (setq mac-option-key-is-meta nil)
 (setq mac-command-key-is-meta t)
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier nil)
 
-;; Get rid of annoying auto-save files
-(defvar user-temporary-file-directory
-  (concat temporary-file-directory user-login-name "/"))
-(make-directory user-temporary-file-directory t)
-(setq backup-by-copying t)
-(setq backup-directory-alist
-      `(("." . ,user-temporary-file-directory)
-        (,tramp-file-name-regexp nil)))
-(setq auto-save-list-file-prefix
-      (concat user-temporary-file-directory ".auto-saves-"))
-(setq auto-save-file-name-transforms
-      `((".*" ,user-temporary-file-directory t)))
