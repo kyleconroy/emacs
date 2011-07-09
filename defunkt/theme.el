c58d3193ae03c7589d50431615d88155ce571b85
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-07-09 00:56:54 -0700

    Solarize my emacs

--- defunkt/theme.el
+++ defunkt/theme.el
@@ -3,5 +3,9 @@
 (require 'color-theme)
 (setq color-theme-is-global t)
 
-(load-file "~/.emacs.d/vendor/twilight-emacs/color-theme-twilight.el")
-(color-theme-twilight)
+;;(load-file "~/.emacs.d/vendor/twilight-emacs/color-theme-twilight.el")
+;;(color-theme-twilight)
+(add-to-list 'load-path "~/.emacs.d/vendor/solarized-emacs")
+(load-file "~/.emacs.d/vendor/solarized-emacs/color-theme-solarized.el")
+(color-theme-solarized-dark)
+
